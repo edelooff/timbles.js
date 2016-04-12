566fc56f5926b9419cf1485bce11df09db29c976
Elmer de Looff <elmer.delooff@gmail.com>
2016-04-12 22:03:20 +0200

    Include extensions as jshint/code style targets.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,10 @@ module.exports = function( grunt ) {
   grunt.initConfig( {
     pkg: grunt.file.readJSON( 'package.json' ),
     jshint: {
-      all: [ 'Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js' ]
+      all: [ 'Gruntfile.js', 'timbles*.js', 'tests/test-scripts/*.js' ]
     },
     jscs: {
-      src: [ 'Gruntfile.js', 'timbles.js', 'tests/test-scripts/*.js' ],
+      src: [ 'Gruntfile.js', 'timbles*.js', 'tests/test-scripts/*.js' ],
       options: {
         config: '.jscsrc',
         verbose: true // Create output with rule names
